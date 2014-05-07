618808de11b16a6124a6916e01ed4af5dd1514ee
kobrinartem <kobrin.artem@gmail.com>
2014-05-07 21:49:21 +0300

    allow 443 deny 80

--- manifests/firewall.pp
+++ manifests/firewall.pp
@@ -1,9 +1,9 @@
 class jboss_cluster::firewall { 
   if $jboss_cluster::firewall {
-    firewall { '80 allow jboss cluster':
+    firewall { '443 allow jboss cluster':
       chain   => 'INPUT',
       state   => ['NEW'],
-      dport   => '80',
+      dport   => '443',
       proto   => 'tcp',
       action  => 'accept',
     }
